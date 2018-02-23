f4503e786cdac7f788e8fad2230b977619f1804c
leviv <villarreallevi@gmail.com>
2018-02-23 16:56:16 -0600

    Update atrticles

---
+++ articles/battery-that-charges-in-one-minute/data.php
@@ -0,0 +1,26 @@
+<?php
+$identifier = 'trust';
+$title = 'Battery That Charges in One Minute Could Beat Tesla in the Race for Tomorrow\'s Clean Car';
+$author = 'Jim Phipps';
+$pubdate = 'Feb. 23, 2017';
+
+$featured_img = '../../dist/img/soldier.jpg';
+$featured_img_caption = '';
+$article = '<p>Henrik Fisker, a former chief designer for Aston Martin, claims that he will soon revolutionize the world of electric cars with a new, superior battery. Fisker started his own company and has filed patents on a new type of solid-state battery, which can be charged faster and has greater capacity than leading lithium-ion batteries, at least in theory.</p>
+<p>In lithium-ion batteries, current flows through either liquids or polymers, but in solid-state batteries it flows through a solid. Although the technology is still in its infancy, these new batteries are designed to be cheaper and smaller than their lithium-ion counterpart.</p> 
+<p>For now, the details of this design remain undisclosed. However, it will be able to hold 2.5-times the energy of a traditional lithium-ion battery, and it will charge at a much faster rate. Fisker even suggests that this battery could be charged in as little as one minute.</p>
+<p>According to Fisker, “the unique thing about our battery is that the solvent that we use to build the battery is the cheapest one you can get – water.”</p>
+<p>So far, the company has only showcased the battery at the Consumer Electronics Show. But, should it amount to a commercially-viable product, the lithium-ion batteries that currently power electric vehicles (EVs) could give way to these batteries and significantly advance EV capabilities and range.</p>
+';
+
+$corrections = '<p>None</p>';
+$who_spoke_to = array(
+    ''
+);
+$where_written = '<p>This story was written on assignment in San Francisco, California</p>';
+$editor = '<p>Marlene Bryant, science editor at <em>The News Beat</em></p>';
+$version_history = '<p>There are no older versions of this story.</p>';
+
+$comments = array();
+
+?>

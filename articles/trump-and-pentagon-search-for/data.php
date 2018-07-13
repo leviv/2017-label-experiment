27c4087aae5ed4e9ad7a8c213ae615538796d105
Levi V <villarreallevi@gmail.com>
2018-07-13 18:38:25 -0500

    Update article due to current events

--- articles/trump-and-pentagon-search-for/data.php
+++ articles/trump-and-pentagon-search-for/data.php
@@ -6,12 +6,12 @@ $pubdate = 'June 5, 2018';
 
 $featured_img = '../../dist/img/soldier.jpg';
 $featured_img_caption = '';
-$article = '<p>A Pentagon plan to send more U.S. troops to Afghanistan aims to halt advances by Taliban insurgents in the short term, but the broader goal is to find a political path to end America’s longest war.</p>
-<p>Defense Secretary Jim Mattis said the Pentagon is formulating a new approach to the war, now in its 16th year. He said that a new strategy would consider whether to bring “more diplomatic and economic pressures on surrounding nations” to help end the conflict.</p> 
-<p>The Pentagon has called the fighting a stalemate between the U.S.-backed Afghan government and the Taliban. Gen. John Nicholson said a few thousand more U.S. troops would be required to turn the tide but would not alter the current military approach, which is to support Afghan forces. </p>
+$article = '<p>The Pentagon plan to send more U.S. troops to Afghanistan aimed to halt advances by Taliban insurgents in the short term, but the broader goal is to find a political path to end America’s longest war.</p>
+<p>Defense Secretary Jim Mattis said the Pentagon is formulating a new approach to the war, now in its 16th year. He said that a new strategy would consider whether to bring “more diplomatic and economic pressures on surrounding nations” to help end the conflict.</p>
+<p>The Pentagon has called the fighting a stalemate between the U.S.-backed Afghan government and the Taliban. Gen. John Nicholson said the few thousand more U.S. troops required to turn the tide would not alter the current military approach, which is to support Afghan forces. </p>
 <p>The Taliban represents an extreme version of Islam and is benefiting from a lack of popular support for the central government, which is widely viewed as inept, corrupt and a puppet of the United States.</p>
 <p>Recent Taliban gains are a dangerous sign, said Seth Jones, an analyst at RAND Corp. “If the Taliban can hold rural areas, it provides them with a sanctuary and eventually they can move on to urban areas,” he said.</p>
-<p>The administration is trying to develop a broader strategy for the region. Insurgencies cannot be defeated on the battlefield alone, Jones said.</p>
+<p>The administration is developing a broader strategy for the region. Insurgencies cannot be defeated on the battlefield alone, Jones said.</p>
 ';
 
 $corrections = '<p>None</p>';
